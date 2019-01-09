b9f11522e33cf0870f4c3a9b453922d3c2876a5d
ivan <ivan.glova@s2is.hr>
2019-01-09 10:00:29 +0100

    Initial Project Add

---
+++ src/main/java/glo/springframework/ggpetclinic/GgPetClinicApplication.java
@@ -0,0 +1,15 @@
+package glo.springframework.ggpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgPetClinicApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(GgPetClinicApplication.class, args);
+	}
+
+}
+
