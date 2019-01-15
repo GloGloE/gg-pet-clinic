7b4ae84027bc0b46bfb96b89dd1e188eb66eff77
ivan <ivan.glova@s2is.hr>
2019-01-15 14:05:30 +0100

    fixing release:prepare and release:perform

--- pet-clinic-web/src/test/java/glo/springframework/ggpetclinic/GgPetClinicApplicationTest.java
+++ pet-clinic-web/src/test/java/glo/springframework/ggpetclinic/GgPetClinicApplicationTest.java
@@ -1,13 +1,15 @@
-package ggpetclinic;
+package glo.springframework.ggpetclinic;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@ContextConfiguration(classes={GgPetClinicApplication.class})
 @SpringBootTest()
-public class GgPetClinicApplicationTests {
+public class GgPetClinicApplicationTest {
 
 	@Test
 	public void contextLoads() {
