4d7bfaad4096ddeda13e0d69935ebe02814ad708
ivan <ivan.glova@s2is.hr>
2019-01-17 13:26:14 +0100

    Add pet clinic index and controller. Closes #8

--- pet-clinic-web/src/main/java/glo/springframework/ggpetclinic/GgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/glo/springframework/ggpetclinic/GgPetClinicApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GgPetClinicApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(GgPetClinicApplication.class, args);
 	}
 
